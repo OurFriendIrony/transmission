e2dcd4229a7724eae8c3ab6240070758ec54253e
Steve Stevens <steve.h.i.stevens@gmail.com>
2022-10-02 17:51:25 +0100

    tidy up js

--- migrate-downloaded-torrents.js
+++ migrate-downloaded-torrents.js
@@ -1,38 +1,57 @@
-const fs = require('fs').promises
-const path = require('path')
+const fs    = require('fs').promises
+const path  = require('path')
 const shell = require('shelljs')
 
 //==================================================
 
-const src_new_media = "/home/steve/media/new"
-const dest_tv = "/home/steve/media/tvshows"
-const dest_movie = "/home/steve/media/films"
-const ext = ["avi", "mkv", "mp4", "srt", "mpg"]
+const SRC_NEW_MEDIA = "/home/steve/media/new"
+const DEST_TV       = "/home/steve/media/tvshows"
+const DEST_FILM     = "/home/steve/media/films"
+const VALID_EXTENSIONS = [
+  "avi",
+  "mkv",
+  "mp4",
+  "srt",
+  "mpg"
+]
+const TRANSMISSION_STATES = [
+  "Seeding",
+  "Stopped",
+  "Finished",
+  "Idle"
+]
 
 //==================================================
 
 async function walk(dir, fileList = []) {
   const files = await fs.readdir(dir)
   for (const file of files) {
+
     const stat = await fs.stat(path.join(dir, file))
+
     if (stat.isDirectory()) {
+      // Directory - recurse
       fileList = await walk(path.join(dir, file), fileList)
+
     } else {
       payload = {}
-      payload["file"] = file
-      payload["src"] = dir
+      payload["file"]     = file
+      payload["ext"]      = file.split(".").pop()
+      payload["garbage"]  = !VALID_EXTENSIONS.includes(payload["ext"])
+      payload["src"]      = dir
 
+      // Identify if this is a film or tvshow
       regex = /(?<name>.*)[S|s](?<season>\d{2})[E|e](?<episode>\d{2}).*/
       m = file.match(regex)
       if (m == null) {
-        payload["dest"] = dest_movie
+        payload["dest"] = DEST_FILM
       } else {
         series = m.groups['name'].replace(/["."]/g,' ').trim()
         season = parseInt(m.groups['season'])
-        payload["dest"] = `${dest_tv}/${series}/Season ${season}`
+        payload["dest"] = `${DEST_TV}/${series}/Season ${season}`
       }
-      payload["ext"] = file.split(".").pop()
-      payload["garbage"] = !ext.includes(payload["ext"])
+
+      // Add to identified resources
       fileList.push(payload)
     }
   }
@@ -42,7 +61,9 @@ async function walk(dir, fileList = []) {
 
 //==================================================
 
-walk(src_new_media).then((resources) => {
+
+
+walk(SRC_NEW_MEDIA).then((resources) => {
   resources.forEach((resource) => {
     if (resource["garbage"]){
       shell.rm(`${resource["src"]}/${resource["file"]}`)
